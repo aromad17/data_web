ea478c54e7508ab327f563c4d1f8281a12d86054
aromad17 <aromad1117@gmail.com>
2024-02-05 01:01:07 +0900

    평균값구해야함2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import { FaLock, FaStar, FaRocket, FaPlus, FaFilter } from "react-icons/fa";
 import {
@@ -17,6 +17,10 @@ import Link from "next/link";
 import ChartComponent from "./components/chart";
 
 export default function Home() {
+  const [createModal, setCreateModal] = useState<boolean>(false);
+  const [projectName, setProjectName] = useState<string>("");
+  const [projectDescription, setProjectDescription] = useState<string>("");
+  const [selectedFile, setSelectedFile]: any = useState(null);
   const [getProject, setGetProject] = useState<any>(null);
   const [projectDetail, setProjectDetail] = useState<any>(null);
   const [projectInfo, setProjectInfo]: any = useState(undefined);
@@ -42,6 +46,60 @@ export default function Home() {
       console.error("에러사항: ", error);
     }
   };
+  // 프로젝트생성@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  const onNewProject = () => {
+    setCreateModal(true);
+  };
+
+  const onChangeProjectName = (e: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+    setProjectName(value);
+  };
+
+  const onChangeProjectDescription = (e: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+    setProjectDescription(value);
+  };
+
+  const onFileChange = (e: any) => {
+    setSelectedFile(e.target.files[0]);
+  };
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData();
+      const config = {
+        projectName: projectName,
+        description: projectDescription,
+      };
+      const configString = JSON.stringify(config);
+      formData.append("projectConfig", configString);
+      formData.append("dataFile", selectedFile);
+
+      const response = await fetch(
+        "http://3.36.0.92:8888/analysis/newProject",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      if (response.ok) {
+        console.log("데이터가 성공적으로 전송되었습니다.");
+        setCreateModal(false);
+        location.reload();
+      } else {
+        console.error("데이터 전송에 실패했습니다.");
+      }
+    } catch (error) {
+      console.error("데이터 전송 중 오류가 발생했습니다:", error);
+    }
+  };
+  // 프로젝트생성@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
   // my project 클릭했을떄
   const pickProject = async (num: number) => {
@@ -100,6 +158,63 @@ export default function Home() {
 
   return (
     <>
+      {createModal ? (
+        <div className={styles.create_modal}>
+          <form className={styles.create_form} onSubmit={onSubmit}>
+            <button
+              className={styles.close_btn}
+              onClick={() => {
+                setCreateModal(false);
+              }}
+            >
+              <FaRegCircleXmark />
+            </button>
+            <fieldset>
+              <label>Create Project</label>
+              <div>
+                Project Name
+                <input
+                  name="project_name"
+                  type="text"
+                  placeholder="Type Project Name"
+                  value={projectName}
+                  onChange={onChangeProjectName}
+                  required
+                />
+              </div>
+
+              <div>
+                Project Description
+                <input
+                  name="project_description"
+                  type="text"
+                  placeholder="Text generation with transformers"
+                  value={projectDescription}
+                  onChange={onChangeProjectDescription}
+                />
+              </div>
+              <div>
+                Project Data
+                <input
+                  name="project_data"
+                  type="file"
+                  placeholder="Upload your Data File"
+                  accept=".csv, .xlsx, .xls"
+                  required
+                  onChange={onFileChange} // 파일 선택 시 실행될 함수
+                />
+              </div>
+              <input
+                type="submit"
+                value="Create Project"
+                className={styles.submit_btn}
+              />
+            </fieldset>
+          </form>
+        </div>
+      ) : (
+        <></>
+      )}
       <div className={styles.wrap} style={{ ...containerStyle }}>
         <div className={styles.tab}>
           <Link legacyBehavior href="/">
@@ -130,6 +245,12 @@ export default function Home() {
                   <></>
                 )}
               </ul>
+            </dd>
+            <dd>
+              <div>
+                <FaPlus style={{ marginRight: "10px" }} />
+                <span onClick={onNewProject}>Create New Project</span>
+              </div>
             </dd>
           </dl>
           <dl>
@@ -227,54 +348,59 @@ export default function Home() {
 
         <div className={styles.contents}>
           <div className={styles.data_visual}>
-            <div className={styles.data_type}>TABLE</div>
-            {getDataName && getDataValue != undefined ? (
-              <>
-                <div className={styles.data_table}>
-                  <table>
-                    <thead>
-                      <tr>
-                        {getDataName != undefined ? (
-                          getDataName.map((name: string, idx: number) => (
-                            <th key={idx}>{name}</th>
-                          ))
-                        ) : (
-                          <></>
-                        )}
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {getDataValue &&
-                        getDataValue.map((row: any, index: number) => (
-                          <tr key={index}>
-                            {getDataName &&
-                              getDataName.map(
-                                (column: string, index: number) => (
-                                  <td key={index}>
-                                    {row[column] !== null ? row[column] : "N/A"}
-                                  </td>
-                                )
-                              )}
-                          </tr>
-                        ))}
-                    </tbody>
-                  </table>
-                </div>
-              </>
-            ) : (
-              <></>
-            )}
+            <div className={styles.data_chart}>
+              <div className={styles.data_type}>TABLE</div>
+              {getDataName && getDataValue != undefined ? (
+                <>
+                  <div className={styles.data_table}>
+                    <table>
+                      <thead>
+                        <tr>
+                          {getDataName != undefined ? (
+                            getDataName.map((name: string, idx: number) => (
+                              <th key={idx}>{name}</th>
+                            ))
+                          ) : (
+                            <></>
+                          )}
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {getDataValue &&
+                          getDataValue.map((row: any, index: number) => (
+                            <tr key={index}>
+                              {getDataName &&
+                                getDataName.map(
+                                  (column: string, index: number) => (
+                                    <td key={index}>
+                                      {row[column] !== null
+                                        ? row[column]
+                                        : "N/A"}
+                                    </td>
+                                  )
+                                )}
+                            </tr>
+                          ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
+            </div>
 
-            <div className={styles.data_type}>CHART</div>
-
-            {getDataName && getDataValue != undefined ? (
-              // 다되면
-              // <ChartComponent dataName={getDataName} dataValue={평균값넣은배열}/>
-              //conponents -> chart.tsx로 이동...
-              <ChartComponent dataName={getDataName} />
-            ) : (
-              <></>
-            )}
+            <div className={styles.data_chart}>
+              <div className={styles.data_type}>CHART</div>
+              {getDataName && getDataValue != undefined ? (
+                // 다되면
+                // <ChartComponent dataName={getDataName} dataValue={평균값넣은배열}/>
+                //conponents -> chart.tsx로 이동...
+                <ChartComponent dataName={getDataName} />
+              ) : (
+                <></>
+              )}
+            </div>
           </div>
         </div>
       </div>
